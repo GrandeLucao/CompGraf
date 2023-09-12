52ff18a05416c9bfa175155c7fa513849690c171
Lucas Gabriel Francener <lucas.francener@tecpuc.com.br>
2023-09-12 12:03:25 -0300

    Texturas quase finalizadas, deu problema com FIleSystem

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -1,6 +1,8 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <shader/shader_s.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image/stb_image.h"
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -36,10 +38,17 @@ int main()
     Shader myShader("shader.vs","shader.fs");
 
     float vertices[] = {
-        // positions         // colors
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+    // positions          // colors           // texture coords
+     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
+    };
+
+    float textCoords[]={
+        0.0f, 0.0f,
+        1.0f,0.0f,
+        0.5f,1.0f
     };
 
     unsigned int VBO, VAO, EBO;
@@ -58,6 +67,34 @@ int main()
     glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float), (void*)(3*sizeof(float)));
     glEnableVertexAttribArray(1);
 
+    glVertexAttribPointer(2,2,GL_FLOAT,GL_FALSE,8*sizeof(float),(void*)(6*sizeof(float)));
+    glEnableVertexAttribArray(2);
+
+    //Texture
+    unsigned int texture;
+    glGenTextures(1,&texture);
+    glBindTexture(GL_TEXTURE_2D,texture);
+
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_MIRRORED_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_MIRRORED_REPEAT);
+
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+
+    int width,height,nrChannels;
+    unsigned char *data=stbi_load(FileSystem::getPath("textures/container.jpg").c_str(), &width,&height,&nrChannels,0);
+    if(data)
+    {
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout<<"Failed to load texture"<<std::endl;
+    }
+    stbi_image_free(data);
+
+
     //Polygon Mode = If able, show only outlines of the figures draw. Almost a test/debug feature of OpenGl
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
@@ -74,8 +111,9 @@ int main()
         myShader.use();
         myShader.setFloat("someUniform",1.0f);
 
+        glBindTexture(GL_TEXTURE_2D,texture);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,0);
 
         //Events and Buffer Swap
         glfwSwapBuffers(window);
