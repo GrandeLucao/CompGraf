ca89b61c9a4bca804bdb54d8007b3241ac44029a
Lucas Gabriel Francener <lucas.francener@tecpuc.com.br>
2023-09-20 10:22:05 -0300

    Transform COmpleto

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -1,7 +1,13 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb_image.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <shader/shader_s.h>
+
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -139,6 +145,11 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
+        //Transforms
+        glm::mat4 trans=glm::mat4(1.0f);
+        trans=glm::translate(trans,glm::vec3(0.5f,-0.5f,0.0f));
+        trans=glm::rotate(trans,(float)glfwGetTime(),glm::vec3(0.0f,0.0f,1.0f));
+
 
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D,texture1);
@@ -146,6 +157,9 @@ int main()
         glBindTexture(GL_TEXTURE_2D,texture2);
 
         myShader.use();
+        unsigned int transformLoc=glGetUniformLocation(myShader.ID,"transform");
+        glUniformMatrix4fv(transformLoc,1,GL_FALSE,glm::value_ptr(trans));
+
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT,0);
 
